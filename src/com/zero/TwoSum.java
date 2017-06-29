e51b6ee4acc032de4e59e0bd929b545feb7385e2
BreakZero <dejin.po@gmail.com>
2017-06-29 09:34:34 +0800

    Initial commit

---
+++ src/com/zero/TwoSum.java
@@ -0,0 +1,51 @@
+package com.zero;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by Zero on 2017/6/29.
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Example :
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[]{3, 2, 4, 5, 1}, 6);
+        for (int i = 0; i < result.length; i ++) {
+            System.out.println(result[i]);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    if (!map.containsKey(i)) {
+                        map.put(i, i);
+                    }
+                    if (!map.containsKey(j)) {
+                        map.put(j, j);
+                    }
+                }
+            }
+        }
+        Iterator it = map.entrySet().iterator();
+        int index = 0;
+        int[] result = new int[map.size()];
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            result[index] = (int) entry.getValue();
+            index++;
+        }
+        return result;
+    }
+}
